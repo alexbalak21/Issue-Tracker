a1d0bae78158759c56fcdc8a4b75b7dfa59d3c14
alexblack24 <alex.black@mail.com>
2024-04-22 22:41:24 +0200

    user comopnents

---
+++ app/components/button.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from "react"
+
+function UserCard() {
+    return (
+        <div>
+            <button onClick={() => console.log("Cicked Button")}>Click</button>
+        </div>
+    )
+}
+
+export default UserCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image"
 import Link from "next/link"
+import UserCard from "./components/button"
 
 export default function Home() {
     return (
         <main>
             <h1>Home</h1>
             <Link href="/users">Users</Link>
+            <UserCard />
         </main>
     )
 }
