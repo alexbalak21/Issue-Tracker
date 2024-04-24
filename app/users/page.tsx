6a288dbe6baf2c4e78bd1728a6547f2ee2736537
alexblack24 <alex.black@mail.com>
2024-04-24 13:59:59 +0200

    next base built

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,25 @@
 import React from "react"
 
-function UsersPage() {
-    return <div>Users Page</div>
+interface User {
+    id: number
+    name: string
+}
+
+async function UsersPage() {
+    const res = await fetch("http://127.0.0.1:5000/users", {cache: "default"})
+    const users: User[] = await res.json()
+
+    return (
+        <>
+            <h1>Users</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default UsersPage
