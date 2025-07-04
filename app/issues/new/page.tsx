46787f1c43afb9a30dd62b452a3c0fc7c6b2c4fa
Alex Balak <alex.balak@outlook.com>
2025-07-04 22:23:17 +0200

    Added a Spinner and error handling from the backend

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {Button, TextField} from "@radix-ui/themes"
+import {Button, Callout, Spinner, TextField} from "@radix-ui/themes"
 import {Label} from "@radix-ui/react-label"
 import SimpleMDE from "react-simplemde-editor"
 import "easymde/dist/easymde.min.css"
@@ -11,10 +11,14 @@ import {zodResolver} from "@hookform/resolvers/zod"
 import {createIssueSchema} from "@/app/schemas/validationSchemas"
 import {z} from "zod"
 import ErrorMessage from "@/app/components/ErrorMessage"
+import LoadingSpinner from "@/app/components/LoadingSpinner"
+import {useState} from "react"
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
 function NewIssuePage() {
+  const [error, setError] = useState("")
+  const [isSubmitting, setIsSubmitting] = useState(false)
   const router = useRouter()
   const {
     register,
@@ -23,25 +27,39 @@ function NewIssuePage() {
     formState: {errors},
   } = useForm<IssueForm>({resolver: zodResolver(createIssueSchema)})
   return (
-    <form
-      className="max-w-2xl mx-auto mt-16"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data)
-        router.push("/issues")
-      })}>
-      <Label htmlFor="title">Title</Label>
-      <ErrorMessage>{errors.title?.message}</ErrorMessage>
-      <TextField.Root className="mb-4 border border-green-500" id="title" placeholder="Title" {...register("title")} />
+    <div className="max-w-2xl mx-auto">
+      {error && (
+        <Callout.Root color="red" className="mb-4">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="mt-16"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            setIsSubmitting(true)
+            await axios.post("/api/issues", data)
+            router.push("/issues")
+          } catch (error) {
+            setError("An unexpected error occurred.")
+          } finally {
+            setIsSubmitting(false)
+          }
+        })}>
+        <Label htmlFor="title">Title</Label>
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
+        <TextField.Root className="mb-4 border border-green-500" id="title" placeholder="Title" {...register("title")} />
 
-      <Label htmlFor="description">Description</Label>
-      <ErrorMessage>{errors.description?.message}</ErrorMessage>
-      <Controller
-        name="description"
-        control={control}
-        render={({field}) => <SimpleMDE placeholder="Description" id="description" className="mb-4" {...field} />}
-      />
-      <Button>Submit New issue</Button>
-    </form>
+        <Label htmlFor="description">Description</Label>
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+        <Controller
+          name="description"
+          control={control}
+          render={({field}) => <SimpleMDE placeholder="Description" id="description" className="mb-4" {...field} />}
+        />
+        <Button disabled={isSubmitting}>Submit New issue {isSubmitting && <LoadingSpinner />}</Button>
+      </form>
+    </div>
   )
 }
 
