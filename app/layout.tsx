41bac4243082df06e03441a454090ddaf779c11e
Alex Balak <alex.balak@outlook.com>
2025-07-04 15:21:16 +0200

    modified font to Inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import "@radix-ui/themes/styles.css"
+import "./theme-config.css"
 import "./globals.css"
 import type {Metadata} from "next"
 import {Inter} from "next/font/google"
-import {Theme} from "@radix-ui/themes"
+import {Theme, ThemePanel} from "@radix-ui/themes"
 import Navbar from "./Navbar"
 
-const inter = Inter({subsets: ["latin"]})
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+})
 
 export const metadata: Metadata = {
   title: "Issue Tracker",
@@ -14,9 +18,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>
-        <Theme>
+        <Theme accentColor="violet" grayColor="slate">
           <Navbar />
           <main>{children}</main>
         </Theme>
