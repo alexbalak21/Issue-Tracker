3939cca3f614b86fdf7b0f339c0c35b716757aa6
Alex Balak <alex.balak@outlook.com>
2025-07-07 16:33:23 +0200

    begin implement the PUT method to update issue

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -17,3 +17,22 @@ export async function POST(request: NextRequest) {
   })
   return NextResponse.json(newIssue, {status: 201})
 }
+
+//PUT method
+export async function PUT(request: NextRequest) {
+  const body = await request.json()
+  const validation = createIssueSchema.safeParse(body)
+  if (!validation.success) {
+    return NextResponse.json(validation.error.format(), {status: 400})
+  }
+
+  const {id} = body
+  const updatedIssue = await prisma.issue.update({
+    where: {id: parseInt(id)},
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  })
+  return NextResponse.json(updatedIssue, {status: 200})
+}
