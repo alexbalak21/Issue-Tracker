c9b64290a24d2eda8531e21e01612301fefef302
Alex Balak <alex.balak@outlook.com>
2025-07-08 11:56:50 +0200

    added modal for deleting issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,35 @@
-import {Button} from "@radix-ui/themes"
-import {MdDelete} from "react-icons/md"
+"use client"
+
+import {AlertDialog, Button} from "@radix-ui/themes"
 import {RiDeleteBin6Line} from "react-icons/ri"
 
 function DeleteIssueButton({issueId}: {issueId: number}) {
   return (
-    <Button color="red" size="3">
-      Delete Issue <RiDeleteBin6Line />
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red" size="3">
+          <RiDeleteBin6Line /> Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title className="text-center pb-7">Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description className="text-center">
+          Are you sure you want to delete this issue? <br /> This action cannot be undone.
+        </AlertDialog.Description>
+        <div className="flex justify-center gap-40 mt-7">
+          <AlertDialog.Action>
+            <Button variant="solid" color="red" size="3">
+              Delete
+            </Button>
+          </AlertDialog.Action>
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray" size="3">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+        </div>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
